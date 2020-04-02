11666adcab8d6f42470a1a5e2d3e13ac7fb59400
chan-b <bchan@seas.upenn.edu>
2020-04-02 19:09:47 -0400

    Updated the java docs for News.java

--- src/MyAnimeList/News.java
+++ src/MyAnimeList/News.java
@@ -22,34 +22,66 @@ public class News extends MyAnimeListObject {
         super(id, NEWS_PROPERTY_TYPES);
     }
     
+    /**
+     * Gets the author's name of this news article
+     * @return Author's name
+     */
     public String getAuthorName() {
         return getStringValue("author_name");
     }
     
+    /**
+     * Gets the user profile url of the author
+     * @return User profile url
+     */
     public String getAuthorUrl() {
         return getStringValue("author_url");
     }
     
+    /**
+     * Gets the number of comments for this news article
+     * @return Number of comments
+     */
     public int getComments() {
         return getIntegerValue("comments");
     }
     
+    /**
+     * Gets the forum url discussing this news article
+     * @return Forum url
+     */
     public String getForumUrl() {
         return getStringValue("forum_url");
     }
     
+    /**
+     * Gets the image url for this news article
+     * @return Image url
+     */
     public String getImageUrl() {
         return getStringValue("image_url");
     }
     
+    /**
+     * Gets the introduction to this news article
+     * @return Introduction
+     */
     public String getIntro() {
         return getStringValue("intro");
     }
     
+    /**
+     * Gets the url to this news article
+     * @return Url
+     */
     public String getUrl() {
         return getStringValue("url");
     }
     
+    /**
+     * Gets the title of this news article
+     * @return Title
+     */
     public String getTitle() {
         return getStringValue("title");
     }
