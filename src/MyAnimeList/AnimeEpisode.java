e99072e83963953e300eb626c8d9afcb7a3fa646
chan-b <bchan@seas.upenn.edu>
2020-04-02 11:22:47 -0400

    Updated java docs for AnimeEpisode.java

--- src/MyAnimeList/AnimeEpisode.java
+++ src/MyAnimeList/AnimeEpisode.java
@@ -20,44 +20,56 @@ public class AnimeEpisode extends MyAnimeListObject {
     }
     
     /**
-     * @return the animeId
+     * Gets the id of the anime this episode belongs to
+     * @return Id of the anime for this episode
      */
     public int getAnimeId() {
         return getIntegerValue("anime_id");
     }
 
     /**
-     * @return the title
+     * Gets the title of this episode
+     * @return Title of the episode
      */
     public String getTitle() {
         return getStringValue("title");
     }
+    
     /**
-     * @return the titleRomanji
+     * Gets the Romanji title of this episode
+     * @return Romanji title of the episode
      */
     public String getTitleRomanji() {
         return getStringValue("title_romanji");
     }
+    
     /**
-     * @return the videoUrl
+     * Gets the URL for the video preview of this episode
+     * @return URL of the video preview
      */
     public String getVideoUrl() {
         return getStringValue("video_url");
     }
+    
     /**
-     * @return the forumUrl
+     * Gets the URL for the forum thread discussing this episode
+     * @return URL for the forum thread for this episode
      */
     public String getForumUrl() {
         return getStringValue("forum_url");
     }
+    
     /**
-     * @return the filler
+     * Determines whether or not this episode contributes to the anime's storyline
+     * @return True if is a filler episode, False if not
      */
     public boolean isFiller() {
         return getBooleanValue("filler");
     }
+    
     /**
-     * @return the recap
+     * Determines whether or not this episode is a recap of a previous episode
+     * @return True if is a recap episode, False if not
      */
     public boolean isRecap() {
         return getBooleanValue("recap");
