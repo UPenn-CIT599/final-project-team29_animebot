9cc81b7fa7e66858d0764c097c0fe92b255688fb
chan-b <bchan@seas.upenn.edu>
2020-04-02 18:55:37 -0400

    Updated java docs for MyAnimeListMedia.java

--- src/MyAnimeList/MyAnimeListMedia.java
+++ src/MyAnimeList/MyAnimeListMedia.java
@@ -36,26 +36,50 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         media = mediaType;
     }
     
+    /**
+     * Gets this media's background
+     * @return Background
+     */
     public String getBackground() {
         return getStringValue("background").toString();
     }
     
+    /**
+     * Gets the English title for this media
+     * @return English title
+     */
     public String getEnglishTitle() {
         return getStringValue("title_english").toString();
     }
     
+    /**
+     * Gets the number of users who favorited this media
+     * @return Number of users
+     */
     public int getFavorites() {
         return getIntegerValue("favorites");
     }
     
+    /**
+     * Gets the url of the image for this media
+     * @return Image Url
+     */
     public String getImageUrl() {
         return getStringValue("image_url");
     }
     
+    /**
+     * Gets the number of users who added this media to their profile
+     * @return Number of users
+     */
     public int getMembers() {
         return getIntegerValue("members");
     }
     
+    /**
+     * Gets a list of news articles related to this media
+     * @return List of news articles
+     */
     public ArrayList<News> getNews() {
         if (articles == null) {
             setNews();
@@ -64,6 +88,10 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         return articles;
     }
     
+    /**
+     * Gets a list of image url's related to this media
+     * @return List of Image Url's
+     */
     public ArrayList<String> getPictures() {
         if (pictures == null) {
             setPictures();
@@ -72,18 +100,34 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         return pictures;
     }
     
+    /**
+     * Gets the popularity rank for this media
+     * @return Popularity ranking
+     */
     public int getPopularity() {
         return getIntegerValue("popularity");
     }
     
+    /**
+     * Gets the overall rank for this media
+     * @return Ranking
+     */
     public int getRank() {
         return getIntegerValue("rank");
     }
     
+    /**
+     * Gets related media items for this media
+     * @return Related media
+     */
     public JSONObject getRelated() {
         return getJsonValue("related");
     }
     
+    /**
+     * Gets list of reviews for this media
+     * @return List of reviews
+     */
     public ArrayList<Review> getReviews() {
         if (reviews == null) {
             setReviews(1);
@@ -92,32 +136,57 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         return reviews;
     }
     
+    /**
+     * Gets average user score for this media
+     * @return User score
+     */
     public double getScore() {
         return getDoubleValue("score");
     }
     
+    /**
+     * Gets the overall status of the media (ie Currently Airing, Finished)
+     * @return Status
+     */
     public String getStatus() {
         return getStringValue("status");
     }
     
+    /**
+     * Gets the synopsis of the media
+     * @return Synopsis
+     */
     public String getSynopsis() {
         return getStringValue("synopsis");
     }
     
+    /**
+     * Gets the title of the media
+     * @return Title
+     */
     public String getTitle() {
         return getStringValue("title");
     }
     
+    /**
+     * Gets the media type (ie Anime, Manga)
+     * @return Media type
+     */
     public String getType() {
         return getStringValue("type");
     }
     
+    /**
+     * Gets the MyAnimeList profile url for the media
+     * @return Media profile url
+     */
     public String getUrl() {
         return getStringValue("url");
     }
     
     /**
      * Checks the boolean map for key. If map doesn't contain key, attempt to retrieve and update object
+     * @return Boolean value for the key
      */
     protected boolean getBooleanValue(String key) {
         if (!booleanProperties.containsKey(key)) {
@@ -133,6 +202,7 @@ public class MyAnimeListMedia extends MyAnimeListObject {
     
     /**
      * Checks the double map for key. If map doesn't contain key, attempt to retrieve and update object
+     * @return Double value for the key
      */
     protected double getDoubleValue(String key) {
         if (!doubleProperties.containsKey(key)) {
@@ -148,6 +218,7 @@ public class MyAnimeListMedia extends MyAnimeListObject {
     
     /**
      * Checks the json map for key. If map doesn't contain key, attempt to retrieve and update object
+     * @return JSONObject value for the key
      */
     protected JSONObject getJsonValue(String key) {
         if (!jsonProperties.containsKey(key)) {
@@ -163,6 +234,7 @@ public class MyAnimeListMedia extends MyAnimeListObject {
     
     /**
      * Checks the integer map for key. If map doesn't contain key, attempt to retrieve and update object
+     * @return Integer value for the key
      */
     protected int getIntegerValue(String key) {
         if (!integerProperties.containsKey(key)) {
@@ -179,6 +251,7 @@ public class MyAnimeListMedia extends MyAnimeListObject {
     
     /**
      * Checks the string map for key. If map doesn't contain key, attempt to retrieve and update object
+     * @return String value for the key
      */
     protected String getStringValue(String key) {
         if (!stringProperties.containsKey(key)) {
@@ -194,6 +267,7 @@ public class MyAnimeListMedia extends MyAnimeListObject {
     
     /**
      * Retrieve and update object values
+     * @return True if details were successfully updated, False if not
      */
     protected boolean setDetails() {
         String result = MyAnimeList.makeAPICall("/" + media + "/" + id);
@@ -201,6 +275,10 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         return true;
     }
     
+    /**
+     * Updates the list of news articles for this media
+     * @return True if news articles were updated, False if not
+     */
     protected boolean setNews() {
         ArrayList<News> articles;
         if (this.articles == null) {
@@ -234,6 +312,10 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         }
     }
     
+    /**
+     * Updates the list of picture url's related to this media
+     * @return True if picture url's were updated, False if not
+     */
     protected boolean setPictures() {
         ArrayList<String> pictures;
         if (this.pictures == null) {
@@ -261,6 +343,11 @@ public class MyAnimeListMedia extends MyAnimeListObject {
         }
     }
     
+    /**
+     * Updates the list of reviews for this media
+     * @param page Page number of review
+     * @return True if reviews were updated, False if not
+     */
     protected boolean setReviews(int page) {
         ArrayList<Review> reviews;
         if (this.reviews == null) {
