0d14777cc40f83aa8b95856fd396362ae01c32bc
chan-b <bchan@seas.upenn.edu>
2020-04-08 19:55:22 -0400

    added compareTo JUnit test to MangaTest.java

--- animebot/src/test/java/myanimelist/MangaTest.java
+++ animebot/src/test/java/myanimelist/MangaTest.java
@@ -1,6 +1,9 @@
 package myanimelist;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 class MangaTest {
@@ -15,6 +18,15 @@ class MangaTest {
     void testGetSequel() {
         Manga manga = MyAnimeList.getManga(23751);
         assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
+    }
+    
+    @Test
+    void testCompareTo() {
+        Map<Integer, Manga> topManga = MyAnimeList.getTopManga("manga");
+        Manga manga2 = topManga.get(2);
+        Manga manga4 = topManga.get(4);
+        
+        assertEquals(-1, manga4.compareTo(manga2));
     }
 
 }
