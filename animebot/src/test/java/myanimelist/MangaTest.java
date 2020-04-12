cb6f587db1088f44e8b7c14702c33a4741d59a91
Gulcin Ennis <gulcin.ennis@gmail.com>
2020-04-12 12:49:35 -0400

    Completed JUnit tests for Manga.java

--- animebot/src/test/java/myanimelist/MangaTest.java
+++ animebot/src/test/java/myanimelist/MangaTest.java
@@ -5,16 +5,45 @@ import org.junit.jupiter.api.Test;
 
 class MangaTest {
 
-    @Test
-    void testGetPrequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
-    }
+	@Test
+	void testgetChapters() {
+		Manga manga = MyAnimeList.getManga(3468);
+		assertEquals(62, manga.getChapters());
+	}
 
-    @Test
-    void testGetSequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
-    }
+	@Test
+	void testGetPrequel() {
+		Manga manga = MyAnimeList.getManga(23751);
+		assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
+	}
 
+	@Test
+	void testGetSequel() {
+		Manga manga = MyAnimeList.getManga(23751);
+		assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
+	}
+
+	@Test
+	void testGetRecommendations() {
+		Manga manga = MyAnimeList.getManga(16765);
+		assertEquals("The Ravages of Time", manga.getRecommendations().get(3).getTitle());
+	}
+
+	@Test
+	void testGetScoreBy() {
+		Manga manga = MyAnimeList.getManga(25);
+		assertEquals(106,455, manga.getScoredBy());
+	}
+
+	@Test
+	void testGetVolumes() {
+		Manga manga = MyAnimeList.getManga(1706);
+		assertEquals(24, manga.getVolumes());
+	}
+
+	@Test
+	void testGetIsPublishing() {
+		Manga manga = MyAnimeList.getManga(13);
+		assertEquals(true, manga.isPublishing());
+	}
 }
