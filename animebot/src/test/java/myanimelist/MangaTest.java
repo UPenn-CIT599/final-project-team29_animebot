4ad9a0de8ffbf219b2a6d46427a75fae16c76059
chan-b <bchan@seas.upenn.edu>
2020-04-22 14:25:01 -0400

    Removed duplicate method name created when resolving merge conflict

--- animebot/src/test/java/myanimelist/MangaTest.java
+++ animebot/src/test/java/myanimelist/MangaTest.java
@@ -13,18 +13,6 @@ class MangaTest {
 		Manga manga = MyAnimeList.getManga(3468);
 		assertEquals(62, manga.getChapters());
 	}
-
-    @Test
-    void testGetPrequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
-    }
-
-    @Test
-    void testGetSequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
-    }
     
     @Test
     void testCompareTo() {
