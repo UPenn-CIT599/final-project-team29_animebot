14cc8953c2090c11e66ec6c649f263e49a7a06c5
chan-b <bchan@seas.upenn.edu>
2020-04-22 14:21:46 -0400

    Resolved merge conflict with MangaTest.java

--- animebot/src/test/java/myanimelist/MangaTest.java
+++ animebot/src/test/java/myanimelist/MangaTest.java
@@ -1,6 +1,9 @@
 package myanimelist;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 class MangaTest {
@@ -10,6 +13,27 @@ class MangaTest {
 		Manga manga = MyAnimeList.getManga(3468);
 		assertEquals(62, manga.getChapters());
 	}
+
+    @Test
+    void testGetPrequel() {
+        Manga manga = MyAnimeList.getManga(23751);
+        assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
+    }
+
+    @Test
+    void testGetSequel() {
+        Manga manga = MyAnimeList.getManga(23751);
+        assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
+    }
+    
+    @Test
+    void testCompareTo() {
+        Map<Integer, Manga> topManga = MyAnimeList.getTopManga("manga");
+        Manga manga2 = topManga.get(2);
+        Manga manga4 = topManga.get(4);
+        
+        assertEquals(-1, manga4.compareTo(manga2));
+    }
 
 	@Test
 	void testGetPrequel() {
