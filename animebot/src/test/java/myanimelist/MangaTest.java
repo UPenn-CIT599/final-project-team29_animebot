b39e5284ae70008739ce296c2cf3988ce2b5aa36
chan-b <bchan@seas.upenn.edu>
2020-04-23 21:15:30 -0400

    Resolved merge issues with MangaTest.java

--- animebot/src/test/java/myanimelist/MangaTest.java
+++ animebot/src/test/java/myanimelist/MangaTest.java
@@ -8,19 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class MangaTest {
 
-    @Test
-    void testGetPrequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
-    }
-
-    @Test
-    void testGetSequel() {
-        Manga manga = MyAnimeList.getManga(23751);
-        assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
-    }
-    
-    @Test
+ @Test
     void testCompareTo() {
         Map<Integer, Manga> topManga = MyAnimeList.getTopManga("manga");
         Manga manga2 = topManga.get(2);
@@ -29,4 +17,45 @@ class MangaTest {
         assertEquals(-1, manga4.compareTo(manga2));
     }
 
+	@Test
+	void testgetChapters() {
+		Manga manga = MyAnimeList.getManga(3468);
+		assertEquals(62, manga.getChapters());
+	}
+
+	@Test
+	void testGetPrequel() {
+		Manga manga = MyAnimeList.getManga(23751);
+		assertEquals("Monogatari Series: First Season", manga.getPrequel().get(0).getTitle());
+	}
+
+	@Test
+	void testGetSequel() {
+		Manga manga = MyAnimeList.getManga(23751);
+		assertEquals("Monogatari Series: Final Season", manga.getSequel().get(0).getTitle());
+	}
+
+	@Test
+	void testGetRecommendations() {
+		Manga manga = MyAnimeList.getManga(16765);
+		assertEquals("The Ravages of Time", manga.getRecommendations().get(3).getTitle());
+	}
+
+	@Test
+	void testGetScoreBy() {
+		Manga manga = MyAnimeList.getManga(25);
+		assertEquals(106,455, manga.getScoredBy());
+	}
+
+	@Test
+	void testGetVolumes() {
+		Manga manga = MyAnimeList.getManga(1706);
+		assertEquals(24, manga.getVolumes());
+	}
+
+	@Test
+	void testGetIsPublishing() {
+		Manga manga = MyAnimeList.getManga(13);
+		assertEquals(true, manga.isPublishing());
+	}
 }
